e07426c587388b65549c076578c55fd12355d513
veronica <veronica.seo@companywe.co.kr>
2023-10-04 23:38:15 +0900

    Button 추가 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import { Button } from './web'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app_container">
+			<div>
+				<label>Button</label>
+				<Button
+					id='button'
+					size='lg'
+				>Button1</Button>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/web/Button/Button.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Button from './Button'
+
+const meta = {
+	title: 'Web/Button',
+	component: Button,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+	argTypes: {},
+} satisfies Meta<typeof Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+	args: {
+		type: 'primary'
+	},
+}
+
+export const Secondary: Story = {
+	args: {
+		type: 'secondary'
+	},
+}
+
+export const Large: Story = {
+	args: {
+		size: 'lg'
+	},
+}
+
+export const Small: Story = {
+	args: {
+		size: 'sm'
+	},
+}
