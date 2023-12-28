ba2dada66c4d353c3e9d8cc5f2366d1c0178ee80
veronica <veronica.seo@companywe.co.kr>
2023-12-29 00:56:48 +0900

    탭 컴포넌트 추가

---
+++ src/Tab/Tab.tsx
@@ -0,0 +1,45 @@
+import React, { CSSProperties } from 'react'
+import classnames from 'classnames'
+import './Tab.scss'
+import { map } from 'lodash-es'
+
+interface TabItem {
+	id: string
+	label: string
+}
+
+interface TabProps {
+  id?: string
+  selected?: string
+  data: Array<TabItem>
+  style?: CSSProperties
+  onClick?: () => void
+}
+
+const Tab = ({
+	id,
+	selected = '01',
+	data = [],
+	style = {},
+	onClick = () => {}
+}: TabProps & React.HTMLAttributes<HTMLButtonElement>) => {
+	
+	return (
+		<div
+			id={id}
+			className={classnames('tab_group_container')}
+			style={style}
+		>
+			{map(data, t => (
+				<div
+					id={`tab-${t.id}`}
+					className={classnames('tab_container', selected === t.id && 'selected')}
+				>
+					{t.label}
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default Tab
