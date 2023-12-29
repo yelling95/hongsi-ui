ea1f3f444115dd8e372c747d6848d35c9c5cf73c
veronica <veronica.seo@companywe.co.kr>
2023-12-29 09:47:04 +0900

    More Options 컴포넌트 추가

---
+++ src/Modal/MoreOption/MoreOption.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import type {StoryObj} from '@storybook/react'
+import MoreOption from './MoreOption'
+
+import {Button} from '../../Button'
+import {range} from 'lodash-es'
+
+const meta = {
+  title: 'Modal/MoreOption',
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  component: MoreOption,
+}
+
+export default meta
+type Story = StoryObj<typeof MoreOption>
+
+const ModalWithHooks = (args: any) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const [selected, setSelected] = React.useState('01')
+
+  return (
+    <div
+      id="modalStoryWrap"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        position: 'relative',
+        width: 375,
+        height: 800,
+        padding: 20,
+        background: '#EFEFEF',
+      }}>
+      <Button onClick={() => setIsOpen(true)}>Open!</Button>
+      <MoreOption
+        isShow={isOpen}
+        isShowDimm={true}
+        selected={selected}
+        close={() => setIsOpen(false)}
+        {...args}
+      />
+    </div>
+  )
+}
+
+export const Default: Story = {
+  render: (args) => ModalWithHooks(args),
+  args: {
+    options: range(1, 11, 1).map((opt) => ({
+      id: opt < 10 ? '0' + opt : '' + opt,
+      label: '아이템 ' + opt,
+      click: () => {
+        alert('아이템 ' + opt)
+      },
+    })),
+  },
+}
---
+++ src/Modal/MoreOption/MoreOption.tsx
@@ -0,0 +1,90 @@
+import React, {CSSProperties, ReactElement} from 'react'
+import classNames from 'classnames'
+import {map} from 'lodash-es'
+
+import './MoreOption.scss'
+
+interface Option {
+  id: string
+  label: string
+  click: () => void
+}
+
+type MoreOptionProps = {
+  isShow?: boolean
+  isShowDimm?: boolean
+  selected?: string
+  options?: Array<Option>
+  style?: CSSProperties
+  close?: () => void
+}
+
+export default function MoreOption({
+  isShow = false,
+  isShowDimm = false,
+  selected = '01',
+  options = [],
+  style,
+  close = () => {},
+}: MoreOptionProps) {
+  const csStyle: CSSProperties = style || {}
+
+  React.useEffect(() => {
+    if (isShowDimm) {
+      const dimmElement = document.getElementById('modalDimm')
+      const layer = dimmElement?.dataset.layer || -1
+
+      if (isShow) {
+        if (!dimmElement) {
+          const dimm = document.createElement('div')
+          dimm.id = 'modalDimm'
+          dimm.classList.add('dimm')
+          dimm.style.backgroundColor = 'var(--dimm-layer)'
+          dimm.style.cursor = 'pointer'
+          dimm.dataset.layer = '0'
+
+          const wrap = document.getElementById('modalStoryWrap')
+
+          if (wrap) {
+            wrap.append(dimm)
+            wrap.style.overflow = 'hidden'
+          } else {
+            document.body.append(dimm)
+            document.body.style.overflow = 'hidden'
+          }
+
+          dimm.addEventListener('click', close)
+        } else {
+          dimmElement.dataset.layer = String(+layer + 1)
+          dimmElement.style.display = 'block'
+          document.body.style.overflow = 'hidden'
+        }
+      } else {
+        if (dimmElement) {
+          if (+layer < 1) {
+            dimmElement.style.display = 'none'
+            document.body.style.overflow = 'auto'
+            dimmElement.dataset.layer = '-1'
+          } else {
+            dimmElement.dataset.layer = String(+layer - 1)
+          }
+        }
+      }
+    }
+  }, [isShow])
+
+  return (
+    <div className={classNames('moreopt_container', 'shadow', isShow && 'open')} style={csStyle}>
+      <div className="option_wrap">
+        {map(options, (opt, optIdx) => (
+          <div
+            key={`moreopt-option-${optIdx}`}
+            className={classNames(opt.id === selected && 'selected')}
+            onClick={opt.click}>
+            {opt.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
