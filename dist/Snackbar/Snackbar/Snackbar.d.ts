938b4d6e2e38d9ffd612aae3857f0dc8cd0c8538
veronica <veronica.seo@companywe.co.kr>
2024-10-14 10:26:17 +0900

    스낵바 애니메이션 추가

--- dist/Snackbar/Snackbar/Snackbar.d.ts
+++ dist/Snackbar/Snackbar/Snackbar.d.ts
@@ -2,10 +2,12 @@ import React from 'react';
 import './Snackbar.scss';
 interface SnackbarProps {
     open: boolean;
+    timeout?: number;
     message: string;
     action: string;
     icon: 'Check';
     onAction?: () => void;
+    onTimeout?: () => void;
 }
-declare const Snackbar: ({ open, message, action, icon, onAction, ...props }: SnackbarProps & React.HTMLAttributes<HTMLButtonElement>) => React.JSX.Element;
+declare const Snackbar: ({ open, timeout, message, action, icon, onAction, onTimeout, ...props }: SnackbarProps & React.HTMLAttributes<HTMLButtonElement>) => React.JSX.Element;
 export default Snackbar;
--- src/Snackbar/Snackbar/Snackbar.tsx
+++ src/Snackbar/Snackbar/Snackbar.tsx
@@ -6,22 +6,36 @@ import './Snackbar.scss'
 
 interface SnackbarProps {
   open: boolean
+  timeout?: number
   message: string
   action: string
   icon: 'Check'
   onAction?: () => void
+  onTimeout?: () => void
 }
 
 const Snackbar = ({
   open = true,
+  timeout,
   message = '텍스트',
   action,
   icon,
   onAction = () => {},
+  onTimeout = () => {},
   ...props
 }: SnackbarProps & React.HTMLAttributes<HTMLButtonElement>) => {
+  const [expire, setExpire] = React.useState(false)
+
+  React.useEffect(() => {
+    if (open && timeout) {
+      setTimeout(() => {
+        setExpire(true)
+        onTimeout()
+      }, timeout)
+    }
+  }, [open])
   return (
-    <div className={classnames('snackbar_container', open && 'open')}>
+    <div className={classnames('snackbar_container', open && 'open', expire && 'expire')}>
       <div className="message_wrap">
         {icon && (
           <div className="icon_wrap">
